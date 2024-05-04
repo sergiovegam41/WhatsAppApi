8dacaca2c99bb34839b3d67c62d92f9c32cab425
Victor Elias Barrera Florez <victorcel2005@gmail.com>
2024-05-04 10:02:24 -0500

    Actualizacion librerias

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-const { downloadMediaMessage } = require('@adiwajshing/baileys');
+//const { downloadMediaMessage } = require('@adiwajshing/baileys');
 const fs = require('node:fs/promises');
 const { convertOggMp3 } = require('./services/convert');
 const { voiceToText } = require('./services/whisper');
@@ -7,10 +7,10 @@ const handlerAI = async (ctx) => {
   /**
    * OMITIR
    */
-  const buffer = await downloadMediaMessage(ctx, "buffer");
+//  const buffer = await downloadMediaMessage(ctx, "buffer");
   const pathTmpOgg = `${process.cwd()}/tmp/voice-note-${Date.now()}.ogg`;
   const pathTmpMp3 = `${process.cwd()}/tmp/voice-note-${Date.now()}.mp3`;
-  await fs.writeFile(pathTmpOgg, buffer);
+//  await fs.writeFile(pathTmpOgg, buffer);
   await convertOggMp3(pathTmpOgg, pathTmpMp3);
   const text = await voiceToText(pathTmpMp3);
   return text; //el habla1!!
